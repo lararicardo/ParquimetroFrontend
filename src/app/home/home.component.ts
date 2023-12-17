bd0d01fb1255998fa039719f70e04a7b29286c25
Ricardo Rafael Lara Gomes <ricardolara.ti@gmail.com>
2023-12-17 00:37:45 -0300

    fix: feito ajustes de código

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,15 +2,13 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatGridListModule } from '@angular/material/grid-list';
 
-
 @Component({
-  selector: 'app-root',
+  selector: 'app-home',
   standalone: true,
   imports: [MatToolbarModule, MatGridListModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { HomeComponent } from './app/home/home.component';
+import { AppComponent } from './app/app.component';
 
-bootstrapApplication(HomeComponent, appConfig)
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
